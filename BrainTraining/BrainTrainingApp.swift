be83442e8c9a2045cf425d1731f4a41504d5afa9
seahpark247 <famousjedo@gmail.com>
2025-04-14 09:43:07 -0500

    Initial Commit

---
+++ BrainTraining/BrainTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainingApp.swift
+//  BrainTraining
+//
+//  Created by Seah Park on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainTrainingTests/BrainTrainingTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainingTests.swift
+//  BrainTrainingTests
+//
+//  Created by Seah Park on 4/14/25.
+//
+
+import Testing
+@testable import BrainTraining
+
+struct BrainTrainingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
